7c453d0422ab670c63521ddd1a0cbd6a2ebce62f
berrysa <sberry925@gmail.com>
2015-06-25 11:38:59 -0400

    Custom child serialization based on parent property

--- src/main/java/com/samlikescode/stackoverflow/questions/q31025046/MyClassSerializerModifier.java
+++ src/main/java/com/samlikescode/stackoverflow/questions/q31025046/MyClassSerializerModifier.java
@@ -3,9 +3,12 @@ package com.samlikescode.stackoverflow.questions.q31025046;
 import com.fasterxml.jackson.databind.BeanDescription;
 import com.fasterxml.jackson.databind.JsonSerializer;
 import com.fasterxml.jackson.databind.SerializationConfig;
+import com.fasterxml.jackson.databind.ser.BeanPropertyWriter;
 import com.fasterxml.jackson.databind.ser.BeanSerializerModifier;
 
-    public class MyClassSerializerModifier extends BeanSerializerModifier {
+import java.util.List;
+
+public class MyClassSerializerModifier extends BeanSerializerModifier {
         @Override
         public JsonSerializer<?> modifySerializer(SerializationConfig config, BeanDescription beanDesc, JsonSerializer<?> serializer) {
             if (beanDesc.getBeanClass() == MySpecificClass.class) {
@@ -13,4 +16,9 @@ import com.fasterxml.jackson.databind.ser.BeanSerializerModifier;
             }
             return serializer;
         }
+
+    @Override
+    public List<BeanPropertyWriter> changeProperties(SerializationConfig config, BeanDescription beanDesc, List<BeanPropertyWriter> beanProperties) {
+        return super.changeProperties(config, beanDesc, beanProperties);    //To change body of overridden methods use File | Settings | File Templates.
     }
+}
---
+++ src/main/java/com/samlikescode/stackoverflow/questions/q31034850/IsSecureChildSerializer.java
@@ -0,0 +1,60 @@
+/*
+*  Copyright 2015 Vertafore, Inc.   All rights reserved.
+*
+*  Disclaimers:
+*   This software is provided "as is," without warranty of any kind, express or
+*   implied, including but not limited to the warranties of merchantability,
+*   fitness for a particular purpose and non-infringement.  This source code
+*   should not be relied upon as the sole basis for solving a problem whose
+*   incorrect solution could result in injury to person or property. In no
+*   event shall the author or contributors be held liable for any damages
+*   arising in any way from the use of this software. The entire risk as to the
+*   results and performance of this source code is assumed by the user.
+*
+*   Permission is granted to use this software for internal use only, subject
+*   to the following restrictions:
+*     1.  This source code MUST retain the above copyright notice, disclaimer,
+*         and this list of conditions.
+*     2.  This source code may be used ONLY within the scope of the original
+*         agreement under which this source code was provided and may not be
+*         distributed to any third party without the express written consent of
+*         Vertafore, Inc.
+*     3.  This source code along with all obligations and rights under the
+*         original License Agreement may not be assigned to any third party
+*         without the expressed written consent of Vertafore, Inc., except that
+*         assignment may be made to a  successor to the business or
+*         substantially all of its assets. All parties bind their successors,
+*         executors, administrators, and assignees to all covenants of this
+*         Agreement.
+*
+*   All advertising materials mentioning features or use of this software must
+*   display the following acknowledgment:
+*
+*     Trademark Disclaimer:
+*     All patent, copyright, trademark and other intellectual property rights
+*     included in the source code are owned exclusively by Vertafore, Inc.
+*/
+package com.samlikescode.stackoverflow.questions.q31034850;
+
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.databind.JsonSerializer;
+import com.fasterxml.jackson.databind.SerializerProvider;
+
+import java.io.IOException;
+
+public class IsSecureChildSerializer extends JsonSerializer<Child> {
+    private static final SecureChildSerializer SECURE_CHILD_SERIALIZER = new SecureChildSerializer();
+    private static final InsecureChildSerializer INSECURE_CHILD_SERIALIZER = new InsecureChildSerializer();
+
+    @Override
+    public void serialize(Child value, JsonGenerator gen, SerializerProvider serializers) throws IOException {
+        if (gen.getCurrentValue() instanceof Parent) {
+            Parent parent = (Parent) gen.getCurrentValue();
+            if (parent.isSecure) {
+                SECURE_CHILD_SERIALIZER.serialize(value, gen, serializers);
+            } else {
+                INSECURE_CHILD_SERIALIZER.serialize(value, gen, serializers);
+            }
+        }
+    }
+}
